c8adae40a135790eea8b386e7216783d467b0d71
UTB\dorpma <maxdorper@gmail.com>
2017-02-15 15:47:45 +0100

    Source tree is fun

---
+++ Assets/Scripts/CoopCamera.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoopCamera : MonoBehaviour {
+    private Transform[] m_PlayerTransforms;
+
+    public float OffsetY = 2.0f;
+    public float MinDistance = 7.5f;
+
+    private float m_CameraMaxDistance = 20.0f;
+    [SerializeField]
+    private float m_CameraDistanceDivider = 1.5f;
+    [SerializeField]
+    private float m_CameraSmoothing = 10.0f;
+
+    private Transform m_MinTransformX;
+    private Transform m_MaxTransformX;
+    private Transform m_MinTransformZ;
+    private Transform m_MaxTransformZ;
+
+    private float m_MinX;
+    private float m_MaxX;
+    private float m_MinZ;
+    private float m_MaxZ;
+
+
+    private void Start() {
+        var allPlayers = GameObject.FindGameObjectsWithTag("Player");
+        m_PlayerTransforms = new Transform[allPlayers.Length];
+
+        for (var i = 0; i < allPlayers.Length; i++) {
+            m_PlayerTransforms[i] = allPlayers[i].transform;
+        }
+    }
+
+    private void LateUpdate() {
+        if (m_PlayerTransforms.Length == 0) {
+            return;
+        }
+        m_MinX = m_MaxX = m_PlayerTransforms[0].position.x;
+        m_MinZ = m_MaxZ = m_PlayerTransforms[0].position.z;
+
+        for (var i = 1; i < m_PlayerTransforms.Length; i++) {
+            if (m_PlayerTransforms[i].position.x < m_MinX) {
+                m_MinX = m_PlayerTransforms[i].position.x;
+                m_MinTransformX = m_PlayerTransforms[i];
+            } else if (m_PlayerTransforms[i].position.x > m_MaxX) {
+                m_MaxX = m_PlayerTransforms[i].position.x;
+                m_MaxTransformX = m_PlayerTransforms[i];
+            }
+
+            if (m_PlayerTransforms[i].position.z < m_MinZ) {
+                m_MinZ = m_PlayerTransforms[i].position.z;
+                m_MinTransformZ = m_PlayerTransforms[i];
+            } else if (m_PlayerTransforms[i].position.z > m_MaxZ) {
+                m_MaxZ = m_PlayerTransforms[i].position.z;
+                m_MaxTransformZ = m_PlayerTransforms[i];
+            }
+        }
+
+        var xMiddle = (m_MinX + m_MaxX) / 2.0f;
+        var zMiddle = (m_MinZ + m_MaxZ) / 2.0f;
+        var distance = Mathf.Max(m_MaxX - m_MinX, m_MaxZ - m_MinZ);
+
+        if (distance < MinDistance) {
+            distance = MinDistance;
+        } else if (distance > m_CameraMaxDistance) {
+            distance = m_CameraMaxDistance;
+        }
+        
+        distance /= m_CameraDistanceDivider;
+
+        var targetCamPos = new Vector3(xMiddle, distance, zMiddle - distance);
+
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, m_CameraSmoothing * Time.deltaTime);
+    }
+}
--- Assets/Scripts/Enemies/EnemyBase.cs
+++ Assets/Scripts/Enemies/EnemyBase.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Boo.Lang;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -12,26 +13,44 @@ public class EnemyBase : MonoBehaviour, IEnemy {
     [SerializeField] private float m_Speed;
     [SerializeField] private float m_AttackRange;
     [SerializeField] private float m_AggroRange;
-    [SerializeField] private GameObject m_PlayerGameObject;
+    private GameObject m_ClosestPlayer;
+
+    private GameObject[] m_PlayerTransforms;
 
     private NavMeshAgent m_EnemyAgent;
 
     // Use this for initialization
     private void Start() {
+        var allPlayers = GameObject.FindGameObjectsWithTag("Player");
         EnemyState = EnemyStates_t.IDLE;
         HealthPoints = m_HealthMax;
         m_EnemyAgent = GetComponent<NavMeshAgent>();
+
+        m_PlayerTransforms = new GameObject[allPlayers.Length];
+
+        for (var i = 0; i < allPlayers.Length; i++) {
+            m_PlayerTransforms[i] = allPlayers[i];
+        }
     }
 
     // Update is called once per frame
     private void Update() {
+
+        m_ClosestPlayer = m_PlayerTransforms[0];
+
+        for (var i = 1; i < m_PlayerTransforms.Length; i++) {
+            if (Vector3.Distance(m_PlayerTransforms[i].transform.position, transform.position) <
+                Vector3.Distance(m_ClosestPlayer.transform.position, transform.position)) {
+                m_ClosestPlayer = m_PlayerTransforms[i];
+            }
+        }
         CheckState();
 
         switch (EnemyState) {
             case EnemyStates_t.IDLE:
                 break;
             case EnemyStates_t.CHASE:
-                m_EnemyAgent.destination = m_PlayerGameObject.transform.position;
+                m_EnemyAgent.destination = m_ClosestPlayer.transform.position;
                 break;
             case EnemyStates_t.ATTACK:
                 break;
@@ -70,11 +89,11 @@ public class EnemyBase : MonoBehaviour, IEnemy {
     }
 
     private bool InAttackRange() {
-        return Vector3.Distance(transform.position, m_PlayerGameObject.transform.position) < m_AttackRange;
+        return Vector3.Distance(transform.position, m_ClosestPlayer.transform.position) < m_AttackRange;
     }
 
     private bool InAggroRange() {
-        return Vector3.Distance(transform.position, m_PlayerGameObject.transform.position) < m_AggroRange;
+        return Vector3.Distance(transform.position, m_ClosestPlayer.transform.position) < m_AggroRange;
     }
 
     public void TakeDamage(int amount) {
