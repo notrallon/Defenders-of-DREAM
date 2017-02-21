ae2773533c1054082456dfc606a7e9067d6d3071
UTB\JedhKa <kabi.jedhagen@gmail.com>
2017-02-21 10:23:35 +0100

    Created spawners for all four players as well as an extra for keyboard controls.

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform[] spawnLocation;
+    public GameObject[] whatToSpawnPrefab;
+    public GameObject whatToSpawnClone;
+    //public GameObject prefab;
+    public KeyCode spawnButton;
+    public KeyCode spawnKey;
+
+    void Update()
+    {
+        //If Player presses Start button, the player gets spawned
+        if ((Input.GetKeyDown(spawnButton)) || (Input.GetKeyDown(spawnKey)))
+        {
+            //Only if prefab doesn't exists.
+            if (whatToSpawnClone == null)
+            {
+                spawnPlayer();
+            }
+        }
+    }
+    //Spawn Player prefab at spawnLocation and sets it's rotation.
+    void spawnPlayer()
+    {
+        whatToSpawnClone = Instantiate(whatToSpawnPrefab[0], spawnLocation[0].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+    }
+}
