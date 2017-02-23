4dd61fad152237867797c9b44cc6928b44377362
Erik Konkell <konkell.erik@gmail.com>
2017-02-23 11:29:57 +0100

    Pause Menu added

---
+++ Assets/Scripts/Menu/PauseGame.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+
+    public Transform canvas;
+
+
+    // Update is called once per frame
+    void Start()
+    {
+        canvas.gameObject.SetActive(false);
+    }
+    void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            pause();
+        }
+       
+	}
+    public void pause()
+    {
+        if (canvas.gameObject.activeInHierarchy == false)
+        {
+            canvas.gameObject.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            canvas.gameObject.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+    public void LoadScene(string name)
+    {
+        Application.LoadLevel(name);
+    }
+}
