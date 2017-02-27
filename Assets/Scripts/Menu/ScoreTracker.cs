7fe87be565bb6dfd8914c2dc2f081f3ea0e8fc23
Erik Konkell <konkell.erik@gmail.com>
2017-02-27 14:08:26 +0100

    score system

---
+++ Assets/Scripts/Menu/ScoreTracker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreTracker : MonoBehaviour {
+
+    public static int score;
+    // Use this for initialization
+    Text text;
+
+    void Awake()
+    {
+        text = GetComponent<Text>();
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        text.text = "Score: " + score;
+	}
+}
