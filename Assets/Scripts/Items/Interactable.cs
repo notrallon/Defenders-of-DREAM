622dcc063b295ffd9f98b038bd74e10e3753d707
Richard Norqvist <richard.norqvist@gmail.com>
2017-02-28 14:50:56 +0100

    Fixed highlighting for pickups.

--- Assets/Scripts/Items/Interactable.cs
+++ Assets/Scripts/Items/Interactable.cs
@@ -2,14 +2,18 @@
 
 public class Interactable : MonoBehaviour {
     private Renderer m_Rend;
-    private Color m_DefaultColor;
+    private Color[] m_DefaultColours;
 
     protected GameObject Player;
 
 	// Use this for initialization
     private void Start () {
 		m_Rend = GetComponent<Renderer>();
-        m_DefaultColor = m_Rend.material.color;
+        m_DefaultColours = new Color[m_Rend.materials.Length];
+
+        for (var i = 0; i < m_DefaultColours.Length; i++) {
+            m_DefaultColours[i] = m_Rend.materials[i].color;
+        }
     }
 
     public virtual void Interact() {
@@ -20,7 +24,12 @@ public class Interactable : MonoBehaviour {
         var playerInput = col.GetComponent<PlayerInput>();
 
         if (playerInput.Interact != null) return;
-        m_Rend.material.color = m_DefaultColor * 3;
+
+        for (var i = 0; i < m_DefaultColours.Length; i++) {
+            m_Rend.materials[i].color = m_DefaultColours[i] * 3;
+        }
+
+        //m_Rend.material.color = m_DefaultColor * 3;
         playerInput.Interact = this;
         Player = col.gameObject;
     }
@@ -28,7 +37,11 @@ public class Interactable : MonoBehaviour {
     private void OnTriggerExit(Collider col) {
         if (!col.CompareTag("Player")) return;
 
-        m_Rend.material.color = m_DefaultColor;
+        for (var i = 0; i < m_DefaultColours.Length; i++) {
+            m_Rend.materials[i].color = m_DefaultColours[i];
+        }
+
+        // m_Rend.material.color = m_DefaultColor;
         col.GetComponent<PlayerInput>().Interact = null;
         Player = null;
     }
