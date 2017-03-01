add811c8c5bb507590b21c70f4edb33b4d843c01
UTB\martse <sebastian.martensson@live.se>
2017-03-01 16:16:17 +0100

    WaterGun1 does now fire particles that deal damage to enemies. Added scripts for dealing damage as a particle, using the water gun and tweaked the WatergunStream particle system.

---
+++ Assets/Scripts/ParticleDamage.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDamage : MonoBehaviour {
+
+    public float damage;
+    public GameObject explosion;
+
+    GameObject enemyObject;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnParticleCollision(GameObject col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+
+            Debug.Log("Particle Collision!");
+
+            enemyObject = col.gameObject;
+            var script = enemyObject.GetComponent<IEnemy>();
+            script.TakeDamage(damage);
+
+            GameObject splat = Instantiate(explosion, enemyObject.transform.position, enemyObject.transform.rotation) as GameObject; // Instantiate the particle splash effect
+
+            Destroy(splat, 1.0f); // destroy the splash-system
+
+            //Debug.Log("Enemy is damaged!");
+        }
+    }
+}
