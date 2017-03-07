86fefb62764a3e20e135af208289d4623cf9f892
UTB\martse <sebastian.martensson@live.se>
2017-03-07 15:06:52 +0100

    Commented WaterGun-script and made the sound effect better.

--- Assets/Scripts/Weapons/WaterGun.cs
+++ Assets/Scripts/Weapons/WaterGun.cs
@@ -7,7 +7,8 @@ public class WaterGun : BaseWeapon, IWeapon {
     public ParticleSystem particles;
     public bool isPlaying;
     public float maxVolume = 1f;
-    public float fadeSpeed = 0.02f; 
+    public float maxPitch = 3f;
+    public float fadeSpeed = 0.01f; 
 
     // Use this for initialization
     private void Start () {
@@ -24,18 +25,23 @@ public class WaterGun : BaseWeapon, IWeapon {
 
     private void Update ()
     {
-        //.particleCount
-        // kolla player Input
-        if ((particles.particleCount < 50) && (particles.particleCount > 0) && isPlaying)
+        //unused stuff: --- kolla PlayerInput? --- *Time.deltaTime
+
+        // set pitch and volume of sound effect based on the amount of particles active from WaterGun
+        if ((particles.particleCount < 100) && (particles.particleCount > 0) && isPlaying)
         {
 
-                audioSource.volume -= particles.particleCount*fadeSpeed*Time.deltaTime;
+                audioSource.volume = particles.particleCount*fadeSpeed;
+                audioSource.pitch = 1 + 2*(particles.particleCount * fadeSpeed);
         }
-        else if ((particles.particleCount > 50) && isPlaying)
+        //if it's more than 100 aprticles - just set  the volume and pitch to max
+        else if ((particles.particleCount > 100) && isPlaying)
         {
 
             audioSource.volume = maxVolume;
+            audioSource.pitch = maxPitch;
         }
+        //if 0 active particles, stop audio source and set "isPlaying" to false so that a new loop can be started in Attack-function
         else if (particles.particleCount == 0)
         {
             audioSource.Stop();
@@ -43,6 +49,7 @@ public class WaterGun : BaseWeapon, IWeapon {
         }
     }
 	
+    //Start emitting particles and play sound effect
     public void Attack(Vector3 dir) {
         particles.Emit(1);
 
