aeb90f4e4fbf97ecb89ca30ca04c9564746ba062
UTB\martse <sebastian.martensson@live.se>
2017-03-14 13:57:52 +0100

    HideObjectsFromPlayer is a new script for making objects transparent if they are between player and camera.

---
+++ Assets/Shaders/HideObjectsFromPlayer.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HideObjectsFromPlayer : MonoBehaviour
+{
+
+    public Transform WatchTarget;
+    public LayerMask OccluderMask;
+
+    //This is the material with the Transparent/Diffuse With Shadow shader
+    public Material HiderMaterial;
+
+    private Material[] intMaterials;
+
+    private Dictionary<Transform, Material> _LastTransforms;
+
+    void Start()
+    {
+        _LastTransforms = new Dictionary<Transform, Material>();
+    }
+
+    void Update()
+    {
+
+        //reset and clear all the previous objects
+        if (_LastTransforms.Count > 0)
+        {
+            foreach (Transform t in _LastTransforms.Keys)
+            {
+                t.GetComponent<Renderer>().material = _LastTransforms[t];
+            }
+            _LastTransforms.Clear();
+        }
+
+        //Cast a ray from this object's transform to the watch target's transform.
+
+        RaycastHit[] hits = Physics.RaycastAll(
+            transform.position,
+            WatchTarget.transform.position - transform.position,
+            Vector3.Distance(WatchTarget.transform.position, transform.position),
+            OccluderMask
+        );
+
+
+        //Loop through all overlapping objects and disable their mesh renderer
+        if (hits.Length > 0)
+        {
+            foreach (RaycastHit hit in hits)
+            {
+                if (hit.collider.gameObject.transform != WatchTarget && hit.collider.transform.root != WatchTarget)
+                {
+
+
+                    intMaterials = new Material[hit.collider.gameObject.GetComponent<Renderer>().materials.Length];
+
+                    for (int i = 0; i < intMaterials.Length; i++)
+                    {
+                        _LastTransforms.Add(hit.collider.gameObject.transform, hit.collider.gameObject.GetComponent<Renderer>().materials[i]);
+                        hit.collider.gameObject.GetComponent<Renderer>().material = HiderMaterial;
+
+                        //_LastTransforms.Add(hit.collider.gameObject.transform, hit.collider.gameObject.GetComponent<Renderer>().material);
+                        //hit.collider.gameObject.GetComponent<Renderer>().material = HiderMaterial;
+                    }
+                }
+            }
+        }
+    }
+}
