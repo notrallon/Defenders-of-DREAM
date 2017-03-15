340867e4b8cf0ebf9b03cdec2f14d4e89178a007
UTB\martse <sebastian.martensson@live.se>
2017-03-15 16:43:54 +0100

    Added scripts for killing enemies with a big lamp

---
+++ Assets/Scripts/GhostKillerLight.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostKillerLight : MonoBehaviour {
+
+    private SphereCollider LightCollider;
+    GameObject enemyObject;
+    private float damage = 3000f;
+
+    // Use this for initialization
+    void Start () {
+        LightCollider = gameObject.GetComponent<SphereCollider>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    void OnTriggerEnter(Collider col)
+    {
+        if (!col.gameObject.CompareTag("Enemy")) return;
+        
+            enemyObject = col.gameObject;
+
+            var script = enemyObject.GetComponent<EnemyBase>();
+            script.TakeDamage(damage);
+
+            Destroy(enemyObject);
+        
+    }
+}
--- Assets/Scripts/LampInteraction.cs
+++ Assets/Scripts/LampInteraction.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class LampInteraction : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    private SphereCollider LightCollider;
+    private GameObject[] GreatLamp;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (LightCollider != null)
+        {
+            if (LightCollider.radius < 70)
+            {
+                LightCollider.radius += 15*Time.deltaTime;
+            }
+        }
 	}
 
     void OnTriggerStay(Collider col)
@@ -21,7 +30,16 @@ public class LampInteraction : MonoBehaviour {
             //If someone presses A on any controller or E on keyboard the button is set to having been pressed (true)
             if (Input.GetKeyDown(KeyCode.JoystickButton0) || (Input.GetKeyDown(KeyCode.E)))
             {
-                //STUFF
+
+                LightCollider = GameObject.FindGameObjectWithTag("GreatLamp").GetComponent<SphereCollider>();
+
+                //GreatLamp = GameObject.FindGameObjectsWithTag("GreatLamp");
+
+                //foreach (GameObject Lamp in GreatLamp)
+                //{
+                //    LightCollider = GetComponent<SphereCollider>();
+                //}
+
             }
         }
     }
