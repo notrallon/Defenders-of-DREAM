96a6f536124e51bf9fe09935e9d4482d1b31a581
Erik Konkell <konkell.erik@gmail.com>
2017-03-17 13:41:56 +0100

    start mr Sandman attack

---
+++ Assets/Scripts/MrSandman/Attack_01.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack_01 : MonoBehaviour {
+
+    [SerializeField]
+    private CapsuleCollider collider;
+    float attackDelay = 2.0f;
+    float attackDuration = 2.5f;
+    // Use this for initialization
+    void Start () {
+
+        collider = GetComponent<CapsuleCollider>();
+        collider.enabled = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (attackDelay > 0)
+        {
+            attackDelay -= Time.deltaTime;
+            Debug.Log(attackDelay);
+        }
+        if(attackDelay <= 0)
+        {
+            collider.enabled = true;
+            attackDelay = 0;
+        }
+        if(collider.enabled == true)
+        {
+            attackDuration -= Time.deltaTime;
+            Debug.Log(attackDuration);
+            if(attackDuration <= 0)
+            {
+                Debug.Log("destroed");
+                Destroy(this.gameObject);
+            }
+        }
+
+	}
+}
