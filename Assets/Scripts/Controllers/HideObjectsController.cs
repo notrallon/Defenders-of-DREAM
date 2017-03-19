4f4f8aac6bd2895d79b7aa07a703e902097bce85
Richard Norqvist <richard.norqvist@gmail.com>
2017-03-19 10:54:41 +0100

    Fixed the bug with hiding objects, and made the camera a bit more flexible.

---
+++ Assets/Scripts/Controllers/HideObjectsController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class HideObjectsController  {
+
+    public static HideObjectsController Instance { get {
+        return m_Instance ?? (m_Instance = new HideObjectsController());
+    } }
+
+    private static HideObjectsController m_Instance;
+
+    public Dictionary<Transform, Material[]> ObjectsToHide = new Dictionary<Transform, Material[]>();
+
+    public void Update() {
+        if (ObjectsToHide.Count > 0) {
+            foreach (Transform t in ObjectsToHide.Keys) {
+                t.GetComponent<Renderer>().materials = ObjectsToHide[t];
+            }
+            ObjectsToHide.Clear();
+        }
+    }
+}
