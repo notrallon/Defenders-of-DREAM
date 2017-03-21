4f9bb26079c5b1dc868018889fadebbdd06dc2b3
Richard Norqvist <richard.norqvist@gmail.com>
2017-03-21 10:59:02 +0100

    Fixed camera shake scripts.

--- Assets/Scripts/Camera/CameraShaker.cs
+++ Assets/Scripts/Camera/CameraShaker.cs
@@ -1,50 +1,57 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraShaker : MonoBehaviour {
 
-    private float m_ShakeTime;
+    //private float m_ShakeTime;
     private float m_CurrentShakeTime;
-
     private Camera m_Camera;
 
-    private Quaternion m_DefaultRot;
-
-    private bool m_IsShaking = false;
+    private bool m_IsShaking;
 
     private float magnitude = 0.2f;
 
+    private const float DEFAULT_MAG = 0.02f;
+    private const float DEFAULT_TIME = 0.5f;
+
 	// Use this for initialization
-	void Start () {
+	private void Start () {
 	    m_Camera = GetComponent<Camera>();
-	    m_DefaultRot = m_Camera.transform.localRotation;
 	}
 
-	// Update is called once per frame
-	void Update () {
-	    if (Input.GetKeyDown(KeyCode.K)) {
-	        Shake(1f);
-	    }
-	}
-
-    public void Shake(float seconds) {
-        m_ShakeTime = seconds;
+    // Shakes the camera with default values
+    public void Shake() {
         m_CurrentShakeTime = 0f;
-        //InvokeRepeating("ShakeCamera", 0, 0.01f);
         if (!m_IsShaking) {
-            StartCoroutine(ShakeIt());
+            StartCoroutine(ShakeIt(DEFAULT_TIME, DEFAULT_MAG));
         }
     }
 
-    IEnumerator ShakeIt() {
+    // Shakes the camera a given amount of seconds, with default magnitude
+    public void Shake(float duration) {
+        m_CurrentShakeTime = 0f;
+        if (!m_IsShaking) {
+            StartCoroutine(ShakeIt(duration, DEFAULT_MAG));
+        }
+    }
+
+    // Shakes the camera for a given amount of seconds and magnitude
+    public void Shake(float duration, float magnitude) {
+        m_CurrentShakeTime = 0f;
+
+        if (!m_IsShaking) {
+            StartCoroutine(ShakeIt(duration, magnitude));
+        }
+    }
+
+    IEnumerator ShakeIt(float seconds, float magnitude) {
         m_IsShaking = true;
         float elapsed = 0.0f;
-        while (elapsed < m_ShakeTime) {
+        while (elapsed < seconds) {
 
             elapsed += Time.deltaTime;
 
-            float percentComplete = elapsed / m_ShakeTime;
+            float percentComplete = elapsed / seconds;
             float damper = 1.0f - Mathf.Clamp(4.0f * percentComplete - 3.0f, 0.0f, 1.0f);
 
             // map value to [-1, 1]
@@ -53,36 +60,10 @@ public class CameraShaker : MonoBehaviour {
             x *= magnitude * damper;
             y *= magnitude * damper;
 
-            Camera.main.transform.position = new Vector3(Camera.main.transform.position.x + x, Camera.main.transform.position.y + y, Camera.main.transform.position.z);
+            m_Camera.transform.position = new Vector3(m_Camera.transform.position.x + x, m_Camera.transform.position.y + y, m_Camera.transform.position.z);
 
             yield return null;
         }
-
         m_IsShaking = false;
-        //Camera.main.transform.position = originalCamPos;
     }
-/*
-    private void ShakeCamera() {
-        m_CurrentShakeTime += Time.deltaTime;
-
-        var t = m_ShakeTime / m_CurrentShakeTime;
-
-        var ShakeX = Random.Range(-0.5f, 0.5f);
-        var ShakeY = Random.Range(-0.5f, 0.5f);
-
-        var targetPos = m_Camera.transform.localRotation;
-
-        targetPos.x = ShakeX;
-        targetPos.y = ShakeY;
-        targetPos.z = 0f;
-
-        m_Camera.transform.localRotation = Quaternion.Lerp(m_Camera.transform.localRotation, targetPos,
-            Time.deltaTime);
-
-        if (m_CurrentShakeTime > m_ShakeTime) {
-            m_CurrentShakeTime = 0f;
-            CancelInvoke("ShakeCamera");
-            m_Camera.transform.localRotation = m_DefaultRot;
-        }
-    }*/
 }
