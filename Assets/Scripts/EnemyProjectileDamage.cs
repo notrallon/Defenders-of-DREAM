c16a2bba1ad3e39d654a575c2203fef1e26c0d88
UTB\dorpma <maxdorper@gmail.com>
2017-03-23 15:04:46 +0100

    ProjectileDamge updated

--- Assets/Scripts/EnemyProjectileDamage.cs
+++ Assets/Scripts/EnemyProjectileDamage.cs
@@ -57,11 +57,11 @@ public class EnemyProjectileDamage : MonoBehaviour
 
             // instantiate particle effect on enemy position
             //GameObject splat = Instantiate(explosion, enemyObject.transform.position, enemyObject.transform.rotation) as GameObject; // Instantiate the particle splash effect
-            for (int i = 0; i < damage; i++)
+            /*for (int i = 0; i < damage; i++)
             {
                 partController.EmitSplatterAtLocation(playerObject.transform, GetComponent<Renderer>().material.color);
             }
-
+            */
             // destroy the splash-system
             //Destroy(splat, 1.0f); 
 
